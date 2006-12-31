e471692dddd44e6faac4013b40284cf18c620a86
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-12-31 22:14:05 +0000

    Made unique indexes more readable. Fixed formatting.

--- db/migrate/004_create_state_deadlines.rb
+++ db/migrate/004_create_state_deadlines.rb
@@ -1,10 +1,10 @@
 class CreateStateDeadlines < ActiveRecord::Migration
   def self.up
     create_table :state_deadlines do |t|
-      t.column :stateful_id,    :integer,   :null => false, :unsigned => true, :references => nil
-      t.column :state_id,       :integer,   :null => false, :unsigned => true
-      t.column :deadline,       :datetime,  :null => false
-      t.column :type,           :string,    :null => false
+      t.column :stateful_id,  :integer,   :null => false, :unsigned => true, :references => nil
+      t.column :state_id,     :integer,   :null => false, :unsigned => true
+      t.column :deadline,     :datetime,  :null => false
+      t.column :type,         :string,    :null => false
     end
     add_index :state_deadlines, [:stateful_id, :state_id], :unique => true
   end
