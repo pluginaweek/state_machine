1c63405692c264d2c2395321b84401152d081a67
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-05-11 17:05:15 +0000

    Use Rails.root instead of RAILS_ROOT in preparation for the Rails 2.1 release

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'plugin_test_helper'
 
 # Run the migrations
-ActiveRecord::Migrator.migrate("#{RAILS_ROOT}/db/migrate")
+ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate")
 
 # Mixin the factory helper
 require File.expand_path("#{File.dirname(__FILE__)}/factory")
