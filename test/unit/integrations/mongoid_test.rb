dd8aee484bc186c461d97040cc0cdecdb24b8989
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2011-03-19 09:48:14 -0400

    Update Mongoid integration test to be dependent on >= 2.0.0.rc.7

--- test/unit/integrations/mongoid_test.rb
+++ test/unit/integrations/mongoid_test.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 # Load library
 require 'rubygems'
 
-gem 'mongoid', ENV['VERSION'] ? "=#{ENV['VERSION']}" : '>=2.0.0'
+gem 'mongoid', ENV['VERSION'] ? "=#{ENV['VERSION']}" : '>=2.0.0.rc.7'
 require 'mongoid'
 
 # Establish database connection
